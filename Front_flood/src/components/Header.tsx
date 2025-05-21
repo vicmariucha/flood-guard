c64389e218487289c54980e994128a91579a11dc
vicmariucha <vicmariucha@gmail.com>
2025-05-20 21:50:40 -0300

    style: atualizações no estilo do header

--- Front_flood/src/components/Header.tsx
+++ Front_flood/src/components/Header.tsx
@@ -15,8 +15,8 @@ function Header({ userName, cityName }: HeaderProps) {
       </div>
       <div className={styles.right}>
         <a href="#" className={styles.link}>Sobre o projeto</a>
-        <button className={styles.iconBtn}><Bell size={18} /></button>
-        <button className={styles.userBtn}><User size={18} /></button>
+        <button className={styles.iconBtn}><Bell size={22} /></button>
+        <button className={styles.userBtn}><User size={22} /></button>
       </div>
     </header>
   );
